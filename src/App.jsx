02f60ddbc2955bab542c32fd39d9cc7469ca18ce
Vinivy Gabriel <vinivygabriel8@gmail.com>
2023-07-20 17:07:51 -0300

    Evaluation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+    import './App.css'
+    import { useState } from 'react'
+    //SVGS
+    import Fav from './Assets/Icons_Main/icon-star.svg'
+    function App() {
+      const { Valor , setValor} = useState()
+      return (
+        <div className="App">
+          <section className="Box">
+           <main>
+             <div className="Header">
+             <div className="Fav">
+               <img src={Fav} alt="" />
+             </div>
+             </div>
+             <div className="Artic">
+               <h1>How did we do?</h1>
+               <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+             </div>
+             <section className="btns">
+               <button value={Valor}>1</button>
+               <button value="2">2</button>
+               <button value="3">3</button>
+               <button value="4">4</button>
+               <button value="5">5</button>
+             </section >
+             <div className="Submit">
+              <button onClick={ () => {
+                alert(`é ${setValor}`)
+              }}>SUBMIT</button>
+             </div>
+           </main>
+          </section>
+        </div>
+      ); 
+    }
 
-function App() {
-  return (
-    <main className="App">
-      
-    </main>
-  );
-}
-
-export default App;
+    export default App;
---
+++ src/Components/FeedBack.jsx
@@ -0,0 +1,19 @@
+import './FeedBack.css'
+function FeedBack() {
+  return (
+    <section className="FeedBack">
+      <div className="image">
+        <img src="" alt="" />
+      </div>
+      <div className="resultado">
+        <p>You select <span></span> out of 5</p>
+      </div>
+      <div className="thanks">
+        <h1>Thank you!</h1>
+        <p>we appreciate you taki8ng the time ti give a rating.if you ever need more support, don't hesitate to get in touch!</p>
+      </div>
+    </section>
+  )
+}
+
+export default FeedBack
